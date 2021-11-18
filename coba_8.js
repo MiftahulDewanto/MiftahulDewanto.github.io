aa2171a9899a0493d0facede234436d9c283e561
Miftahul Dewanto <90891230+MiftahulDewanto@users.noreply.github.com>
2021-11-18 10:08:53 +0700

    coba_8.js
    Pengenalan Java Script

---
+++ coba_8.js
@@ -0,0 +1,29 @@
+console.log("Hello Guys"); 
+
+
+//Prints "Hello Guys"
+console.log("Hello Guys");
+
+
+console.log(70);
+console.log(50+43);
+console.log(3-6);
+
+
+console.log(10+100)
+console.log("10+100")
+
+
+console.log(20*6);
+console.log(200/5);
+
+
+console.log(64%7); //sisa
+
+
+console.log("Jurusan" + "Matematika");
+console.log("B" + "U" + "M" + "N");
+
+
+console.log("20" + "6");
+console.log(20+6);
